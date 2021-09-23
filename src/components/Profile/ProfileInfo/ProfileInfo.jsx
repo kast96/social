8e5b5e7d1048ac982279e109feb2df6cf9729c0c
kast96 <kast1996@gmail.com>
2021-09-23 20:35:47 +0300

    add profile status

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,5 +1,6 @@
 import Preloader from "../../common/Preloader/Preloader";
 import userPhoto from "../../../assets/images/user.png";
+import ProfileStatus from "./ProfileStatus";
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -10,6 +11,7 @@ const ProfileInfo = (props) => {
         <div className="profile__avatar">
           <img src={(props.profile.photos.large != null) ? props.profile.photos.large : userPhoto} alt=""/>
         </div>
+        <ProfileStatus status="123" />
         <div className="profile__description">description</div>
       </div>
     );
